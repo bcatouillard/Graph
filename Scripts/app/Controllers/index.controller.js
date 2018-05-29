37734a87417431b0be3dec9261067ef2e57866b1
CATOUILLARD Benjamin <bcatouillard@ahnac.com>
2018-05-29 15:07:59 +0200

    feat index.controller.js

--- Scripts/app/Controllers/index.controller.js
+++ Scripts/app/Controllers/index.controller.js
@@ -1,11 +1,22 @@
-﻿angular.module('app').controller("IndexController", ["$scope", IndexController = function ($scope) {
+﻿angular.module('app').controller("IndexController", ["$scope", IndexController = function ($scope, IndexService) {
 
     $scope.weight = false;
+       
+    $(document).ready(function(){
+        $.getJSON('weight.json', function(data){
+            $('fullname').empty();
+            $.each(data.fullname, function(entryIndex, entry){
+                $scope.fullname = entry.FULLNAME;
+                
+                
+            });
+        });
+    });
+    
 
     if(!$scope.weight){
         $scope.wght = "HHHHHHH";
     }
-    
     $scope.tension = false;
 
     if(!$scope.tension){
--- Scripts/app/Services/index.service.js
+++ Scripts/app/Services/index.service.js
@@ -1,3 +1,21 @@
 ﻿angular.module('app').factory('IndexService', ['$http', '$q', '$location', function ($http, $q, $location) {
-   
+
+    return{
+        Fullname: function(){
+            var config = {
+                params: "FULLNAME"
+            }
+
+            $http.jsonp('weight.json', config).then(function(response){
+                var fullname = response;
+                return fullname;
+            });
+        }
+
+        ,Personne: function(){
+            $http.jsonp('weight.json').then(function(response){
+                return reponse;
+            });
+        }
+    }
 }]);
