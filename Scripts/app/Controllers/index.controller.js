b6f4194d2db53759c3c25ece67ad9634fbca1332
CATOUILLARD Benjamin <bcatouillard@ahnac.com>
2018-05-29 10:57:08 +0200

    feat index.controller.js

--- Scripts/app/Controllers/index.controller.js
+++ Scripts/app/Controllers/index.controller.js
@@ -1,8 +1,15 @@
 ﻿angular.module('app').controller("IndexController", ["$scope", IndexController = function ($scope) {
 
+    $scope.weight = false;
 
+    if(!$scope.weight){
+        $scope.wght = "HHHHHHH";
+    }
+    
+    $scope.tension = false;
 
-        
-
+    if(!$scope.tension){
+        $scope.tsn = "FFFFF";
+    }
     
 }]);
