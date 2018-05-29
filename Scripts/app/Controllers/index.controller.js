280420603e28a79e7b1b53889a5d153c46dd4296
CATOUILLARD Benjamin <bcatouillard@ahnac.com>
2018-05-29 17:00:04 +0200

    feat index.controller.js

--- Scripts/app/Controllers/index.controller.js
+++ Scripts/app/Controllers/index.controller.js
@@ -1,4 +1,4 @@
-﻿angular.module('app').controller("IndexController", ["$scope", IndexController = function ($scope, $http) {
+﻿angular.module('app').controller("IndexController", ["$scope", IndexController = function ($scope) {
 
     $(document).ready(function(){
         $.ajax("weight.json", {
