f956c66c8127b6c40b01b03fa2307584d3c0e6f8
CATOUILLARD Benjamin <bcatouillard@ahnac.com>
2018-06-04 10:43:54 +0200

    feat index.controller.js

--- Scripts/app/Controllers/index.controller.js
+++ Scripts/app/Controllers/index.controller.js
@@ -1,19 +1,50 @@
-﻿angular.module('app').controller("IndexController", ["$scope", IndexController = function ($scope) {
+﻿angular.module('app').controller("IndexController", ["$scope","$http", IndexController = function ($scope,$http) {
 
-    $scope.weight = false;
+    var label, data, weighthistory;
 
-    $scope.tension = false;
+    $scope.cat = "";
 
-    FusionCharts.ready(function() {
-        var fusioncharts = new FusionCharts({
-            type: 'line',
-            renderAt: 'chartContainer',
-            width: '1500',
-            height: '250',
-            dataFormat: 'jsonurl',
-            dataSource: 'weight.json'
-        });
-        fusioncharts.render();
+    $scope.Categorie = [{
+        id : 1,
+        name : 'Poids'
+    },{
+        id : 2,
+        name : 'Tension'
+    }];
+
+    JSONFile("weight.json", function(text){
+        var data = JSON.parse(text);
+        weighthistory = data.WEIGHTHISTORY;
+    
+        var MyChart = new FusionCharts({});
+        
+        $scope.GetValue = function(categorie){
+            var categorieID = $scope.selectCategorie;
+            var categorieName = $.grep($scope.Categorie, function(categorie){
+                label = categorieName;
+                $scope.cat = label;
+                data = [$scope.selectCategorie]; 
+                MyChart = ({
+                    yAxisName: label,
+                    type: 'line',
+                    renderAt: 'graph',
+                    width: 1500,
+                    height: 250,
+                    dataFormat: 'json',
+                });
+            })
+        };
     });
 
+    function JSONFile(file, callback) {
+        var request = new XMLHttpRequest();
+        request.overrideMimeType("application/json");
+        request.open("GET", file, true);
+        request.onreadystatechange = function() {
+            if (request.readyState === 4 && request.status == "200") {
+                callback(request.responseText);
+            }
+        }
+        request.send(null);
+    }
 }]);
